7c3851387021de63aa4539855e5dae63af3fce53
thatdania@gmail.com <thatdania@gmail.com>
2017-10-09 20:52:19 +0800

    Directory Updated

--- directory.rb
+++ directory.rb
@@ -1,20 +1,27 @@
-student_count = 11
 #printing the list of students
 
+students = [
+
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
 
 puts "The students of Villans Academy"
 puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffery Baratheon"
-puts "Norman Bates"
+
+#this ireteration will print 
+students.each do |student|
+  puts student
+end
 
 #printing the total number of students
-puts  "Overall, we have #{student_count} great students" 
+puts  "Overall, we have #{student.count} great students"
