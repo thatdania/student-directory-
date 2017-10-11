78e0b6aec30a6740a47e0489712a60a3897a4c30
Ignacio Panebianco <ignaciopanebianco@gmail.com>
2017-10-11 14:53:35 +0100

    Update directory.rb

--- directory.rb
+++ directory.rb
@@ -3,40 +3,31 @@ def input_students
   puts "To finish, just hit return twice"
   students = []
   name = gets.chomp
-
-  while !name.empty? do
-  puts "Where is she from?"
-  country = $stdin.gets.chomp
-  break if !country.empty?
+  # It might be better to use an if here because the while will run the code just once as the variable "name" is not changing inside it
+  # while !name.empty? do
+  # puts "Where is she from?"
+  # country = $stdin.gets.chomp
+  # break if !country.empty?
+  # end
+  unless name.empty?
+    puts "where is the student from?"
+    country = gets.chomp
   end
-
-  while !name.empty? && !country.empty? do
-  students << {name: name, country: country , cohort: :October}
-  puts "Now we have #{students.count} students"
-  # get another name from the user
-  name = gets.chomp
+  # I changed the while with ! for an until so that it will evaluate to true and removed the part after the &&
+  # because the country variable will always be empty if the name is empty
+  # while !name.empty? && !country.empty? do
+  # students << {name: name, country: country , cohort: :October}
+  # puts "Now we have #{students.count} students"
+  # name = gets.chomp
+  # end
+  until name.empty?
+    students << {name: name, country: country , cohort: :October}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+    unless name.empty?
+      puts "where is the student from?"
+      country = gets.chomp
+    end
   end
   students
 end
-
-def print_header
-puts "The students of Makers Academy"
-puts "-------------"
-end
-
-def prints(students)
-  students.each do |student|
-    puts "#{student[:name]} #{student[:country]} (#{student[:cohort]} cohort)"
-  end
-end
-
-#printing the total number of students
-def print_footer(names)
-puts "Overall, we have #{names.count} great students"
-end
-
-#method printing
- students = input_students
- print_header
- prints(students)
- print_footer(students)
