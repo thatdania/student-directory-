382d77728f9015f5d551b5d74704d72836ca7d18
thatdania@gmail.com <thatdania@gmail.com>
2017-10-12 09:41:15 +0800

    Well Designed & Detailed Form

--- directory.rb
+++ directory.rb
@@ -1,38 +1,65 @@
+@year_cohorts = [
+  :january,
+  :february,
+  :march,
+  :april,
+  :may,
+  :june,
+  :july,
+  :august,
+  :september,
+  :october,
+  :november,
+  :december
+]
+
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "------------- Student List Form -------------".center(75)
+  puts "Add a student profile to the list. What's his/her name?".center(72)
   students = []
   name = gets.chomp
 
-  while !name.empty? do
-  puts "Where is she from?"
-  country = $stdin.gets.chomp
-  break if !country.empty?
+
+    while !name.empty? do
+     puts "What country are they from?".center(43)
+     country = $stdin.gets.chomp
+
+    puts "What's his or her age?".center(39)
+     age = $stdin.gets.chomp.to_i
+     if age == "0" then puts "nil" end
+
+     puts "What cohort are they in? (month)".center(48)
+     cohort = $stdin.gets.chomp
+    while !(@year_cohorts.include? cohort.downcase.to_sym)
+     puts "Sorry that ain't an answer. Please try again."
+     cohort = $stdin.gets.chomp
   end
 
-  while !name.empty? && !country.empty? do
-  students << {name: name, country: country , cohort: :October}
-  puts "Now we have #{students.count} students"
+   students << {name: name, country: country , age: age, cohort: cohort}
+     puts "Now we have #{students.count} students".center(39)
+     puts "Add another student's profilf his/her name.".center(60)
+     puts "or press ENTER to print out the whole list".center(58)
   # get another name from the user
-  name = gets.chomp
+     name = gets.chomp
   end
-  students
-end
+     students
+  end
 
 def print_header
-puts "The students of Makers Academy"
-puts "-------------"
+puts "The students of Makers Academy".center(47)
+puts "-------------".center(30)
 end
 
 def prints(students)
   students.each do |student|
-    puts "#{student[:name]} #{student[:country]} (#{student[:cohort]} cohort)"
+    puts "#{student[:name]}, country:#{student[:country]} , age :#{student[:age]}, cohort:(#{student[:cohort]})".center(68)
   end
 end
 
 #printing the total number of students
 def print_footer(names)
-puts "Overall, we have #{names.count} great students"
+puts "Overall, we have #{names.count} great students!".center(50)
+puts "---------------------------------------------".center(75)
 end
 
 #method printing
