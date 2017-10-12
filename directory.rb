ccf1deede78849fe2ba14603953b2d6b1cfbd68b
thatdania@gmail.com <thatdania@gmail.com>
2017-10-12 12:15:37 +0800

    If input not entered, its an alien

--- directory.rb
+++ directory.rb
@@ -14,37 +14,52 @@
   :december
 ]
 
+@students = []
 #Inputting the student name, #country and # age
+
 def input_students
   puts "------------- Student List Form -------------".center(60)
   puts "Add a student profile to the list. What's his/her name?".center(72)
-  students = []
-  name = gets.chomp
+
+  name = gets.chomp.downcase.capitalize
+  if name.empty?
+    puts "This is an alien!"
+    exit(true)
+  end
 
     while !name.empty? do
      puts "What country are they from?".center(43)
      country = $stdin.gets.chomp
+     if country.empty?
+       puts "This isan alien!"
+       exit(true)
+     end
 
     puts "What's his or her age?".center(39)
-     age = $stdin.gets.chomp.to_i
-     if age == "0" then puts "nil" end
+     age = $stdin.gets.chomp
+      if age.empty?
+         puts "This is an alien!"
+         exit(true)
+       end
+       age.to_i
 
 #using the @year_cohorts to specify the answer for the cohort
      puts "What cohort are they in? (month)".center(48)
      cohort = $stdin.gets.chomp
+
     while !(@year_cohorts.include? cohort.downcase.to_sym)
      puts "Sorry that ain't an answer. Please try again."
      cohort = $stdin.gets.chomp
   end
 
-   students << {name: name, country: country , age: age, cohort: cohort}
-     puts "Now we have #{students.count} students".center(39)
+  @students << {name: name, country: country , age: age, cohort: cohort}
+     puts "Now we have #{@students.count} students".center(39)
      puts "Add another student's profile his/her name.".center(60)
      puts "or press ENTER to print out the whole list".center(58)
   # get another name from the user
      name = gets.chomp
   end
-     students
+     @students
   end
 
 #Header of the form result
@@ -59,7 +74,7 @@ def prints(students)
   puts "What cohort do you want to see?".center(47)
   c = gets.chomp
   puts
-  students.map do |student| if student[:cohort] == c
+  @students.map do |student| if student[:cohort] == c
     print student[:name]
     print ", Country: "
     print student[:country]
@@ -72,13 +87,17 @@ end
 
 #printing the total number of students and end
 def print_footer(names)
-puts
-puts "Overall, we have #{names.count} great students!".center(50)
+  puts
+if names.count == 1
+  puts "Overall, we have #{names.count} great student!".center(50)
+else
+  puts "Overall, we have #{names.count} great students!".center(50)
+end
 puts "---------------------------------------------".center(60)
 end
 
 #method printing
- students = input_students
- print_header
- prints(students)
- print_footer(students)
+students = input_students
+print_header
+prints(students)
+print_footer(students)
